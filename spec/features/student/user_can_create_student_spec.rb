a513b91af6b273c357e14daad7bb1ce036364496
ClaireBvs <clairembeauvais@gmail.com>
2018-07-10 14:08:27 -0600

    Add feature test create new student

---
+++ spec/features/student/user_can_create_student_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'a user visits new student page' do
+  it 'can create a new student' do
+
+    visit new_student_path
+
+    fill_in :student_name, with: "Jonh"
+    click_button "Create Student"
+
+    expect(current_path).to eq("/students")
+    expect(page).to have_content("John")
+  end
+end
