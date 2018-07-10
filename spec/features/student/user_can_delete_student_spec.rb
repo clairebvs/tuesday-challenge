db21bf2810a9b79f8c5fa55b92f2f6713e28d3b8
ClaireBvs <clairembeauvais@gmail.com>
2018-07-10 15:21:32 -0600

    WIP fixing edit roll back

--- spec/features/student/user_can_delete_student_spec.rb
+++ spec/features/student/user_can_delete_student_spec.rb
@@ -4,7 +4,6 @@ describe 'user visit index student page' do
   it 'can delete a student' do
     student1 = Student.create(name: "Jasmine")
     student2 = Student.create(name: "Jean")
-    student3 = Student.create(name: "Naomie")
 
     visit students_path
 
@@ -12,6 +11,6 @@ describe 'user visit index student page' do
     click_link "Delete"
     expect(current_path).to eq("/students")
     expect(page).to_not have_content(student1.name)
-    expect(page).to have_content(student3.name)
+    expect(page).to have_content(student2.name)
   end
 end
