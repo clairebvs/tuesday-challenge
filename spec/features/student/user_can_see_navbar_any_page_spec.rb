2fc01e1b23b5fa142f165f7796f98d43de7746d4
ClaireBvs <clairembeauvais@gmail.com>
2018-07-10 15:53:45 -0600

    Add validations test for addresses

--- spec/features/student/user_can_see_navbar_any_page_spec.rb
+++ spec/features/student/user_can_see_navbar_any_page_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-describe 'a user can visit any page' do
-  it 'sees a navbar with link to see all students' do
-    
-  end
-end
+# describe 'a user can visit any page' do
+#   it 'sees a navbar with link to see all students' do
+#     student1 = Student.create(name: "Jacob")
+#     student2 = Student.create(name: "Jonah")
+#
+#
+#   end
+# end
